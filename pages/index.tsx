b27d595c315ca09e0f198aa5a431a65a8fc5ecd8
trajashekar423 <trajashekar423@gmail.com>
2024-08-08 15:41:17 +0530

    de

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>My todos2222</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
